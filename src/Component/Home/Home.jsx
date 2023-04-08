0f427f229eaea7280365cf3c8d1fe49861471a0a
ashraful amin <ashrafulamin654@gmail.com>
2023-04-08 07:24:51 +0600

    56-2 Display TShirts and style t-shirt components

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Tshirt from '../Tshirt/Tshirt';
+import './Home.css';
 const Home = () => {
     const tshirts = useLoaderData();
+    const [cart,setCart] = useState([]);
+    const handleAddToCart = tshirt => {
+        console.log(tshirt)
+    }
     return (
-        <div>
-            <h2>this is home {tshirts.length}</h2>
+        <div className='home-container'>
+            <div className="t-shirts-container">
+                {
+                    tshirts.map(tshirt => <Tshirt
+                        key={tshirt._id}
+                        tshirt={tshirt}
+                        handleAddToCart={handleAddToCart}
+                    ></Tshirt>)
+                }
+            </div>
+            <div className="cart-container">
+                <h2>order summery</h2>
+            </div>
         </div>
     );
 };
